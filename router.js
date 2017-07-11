5a4b701c38daa8685607cd4fdaccf26049969bbe
marcedem <marcsea2000@gmail.com>
2017-07-11 14:50:16 +0200

    authenticated request with passport as middleware

--- router.js
+++ router.js
@@ -1,7 +1,15 @@
 const Authentication = require('./controllers/authentication');
+const passportService = require('./services/passport');
+const passport = require('passport');
+
+// if user authenticated, set cookie session to false
+// requireAuth: play the role of a middleware
+const requireAuth = passport.authenticate('jwt', { session: false });
 
 module.exports = function(app){
     //app.get('/', Authentication.allposts);
-
+    app.get('/', requireAuth, function(req, res){
+        res.send({hi: 'there' })
+    });
     app.post('/signup', Authentication.signup);
 }
